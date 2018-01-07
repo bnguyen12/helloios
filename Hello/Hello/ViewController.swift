ae9964e302e707518afbba559cd12ae1860ca56d
Benny Nguyen <benny.nguyenx@gmail.com>
2018-01-06 21:24:51 -0800

    Add the Hello app and screenshots of Hello being used in an iPhone 5s simulator and a personal iPhone X

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Benny Nguyen on 1/6/18.
+//  Copyright © 2018 Benny Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
